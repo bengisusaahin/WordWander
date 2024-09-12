46ac67fd938abd0de10845902ddaab0ecd759716
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-09-12 23:23:50 +0300

    feat: Updated words fragment

--- app/src/main/java/com/bengisusahin/wordwander/ui/words/WordsFragment.kt
+++ app/src/main/java/com/bengisusahin/wordwander/ui/words/WordsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bengisusahin.wordwander.R
 import com.bengisusahin.wordwander.databinding.FragmentWordsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,17 +18,27 @@ class WordsFragment : Fragment() {
     private var _binding: FragmentWordsBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_words, container, false)
+        _binding = FragmentWordsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.wordsRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        viewModel.words.observe(viewLifecycleOwner) { words ->
+            binding.wordsRecyclerView.adapter = WordsAdapter(words)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
